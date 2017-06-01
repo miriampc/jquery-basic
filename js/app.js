93a967fb0478be5a375b881b60fa3cd925d75b49
Miriam Peralta <miiriampc@gmail.com>
2017-06-01 17:45:31 -0500

    pop up with jquery

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-  alert("Cargó jQuery");
-});
+// $(document).ready(function(){
+//   alert("Cargó jQuery");
+// });
 
-$( _ => { //() => {} or function(){}
+// $( _ => { //() => {} or function(){}
   //alert("Cargó jQuery");
-  $("a").click(function(){
+  // $("a").click(function(){
 
-  })//devuelve uno o mas elementos
+  // })//devuelve uno o mas elementos
 
 //selector .on ('click',(e)=>{}) ejecuta el evento para todos los selectores
 
-});
+// });
 
 //JQuery devuelve un array de objetos,
 
@@ -20,3 +20,20 @@ $( _ => { //() => {} or function(){}
 
 
 //Scrapping scanear data de la web
+
+$( () => { //function() {}
+  $(".popup-link").on('click',(e) => {
+    e.preventDefault();
+    $('.brand-popup').show();
+    //$('.brand-popup').removeClass('hide');
+    //$('.brand-popup').addClass('show');
+
+  });
+
+  $(".brand-popup .popup-close").on('click',(e) => {
+    e.preventDefault();
+    $('.brand-popup').hide();
+    //$('.brand-popup').removeClass('show');
+    //$('.brand-popup').addClass('hide');
+  })
+});
