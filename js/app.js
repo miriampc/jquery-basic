b136f48269976c5f365bd3de24aaee816b5b5a1f
Miriam Peralta <miiriampc@gmail.com>
2017-06-01 17:33:09 -0500

    pop up with jquery

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  alert("Cargó jQuery");
+});
+
+$( _ => { //() => {} or function(){}
+  //alert("Cargó jQuery");
+  $("a").click(function(){
+
+  })//devuelve uno o mas elementos
+
+//selector .on ('click',(e)=>{}) ejecuta el evento para todos los selectores
+
+});
+
+//JQuery devuelve un array de objetos,
+
+// $(function(){
+//   alert("Cargó jQuery");
+// });
+
+
+//Scrapping scanear data de la web
